a313dfe918d41fc46ffc24ec8797f5b20ef593fc
Chesters-CZ <bezouskavit@gmail.com>
2020-11-27 15:00:29 +0100

    komit

---
+++ src/LanguageManager.java
@@ -0,0 +1,13 @@
+public enum LanguageManager {
+    INSTANCE;
+
+    private Language language;
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+}
